01635cd089ea6d76a5c2204213c929642b82bd5d
Matt March <matthewmarch29@gmail.com>
2020-06-23 16:50:23 +0100

    Add soundtrack which speeds up with insanity level

--- SoundEffects.gd
+++ SoundEffects.gd
@@ -1,4 +1,7 @@
-extends AudioStreamPlayer
+extends Node
+
+const MAX_MUSIC_PITCH := 2.0
+const MUSIC_PITCH_DELTA = 0.05
 
 # Shotgun_Quick_Pump_01 from: 
 # https://gamesounds.xyz/?dir=Sonniss.com%20-%20GDC%202016%20-%20Game%20Audio%20Bundle/TS%20Sound%20-%2012%20Gauge%20Shotgun
@@ -8,17 +11,19 @@ const reload = preload("res://Assets/Sounds/Reload.wav")
 # https://gamesounds.xyz/?dir=Sonniss.com%20-%20GDC%202017%20-%20Game%20Audio%20Bundle/Gamemaster%20Audio%20-%20%20Gun%20Sound%20Pack
 const gun_shot = preload("res://Assets/Sounds/Shoot.wav")
 
-
 var reloading := false
 
 
 func _ready():
-    connect("finished", self, "_on_finished")
+    $Effects.connect("finished", self, "_on_effects_finished")
+    Events.connect("happy_pig_slain", self, "_on_happy_pig_slain")
+    Events.connect("game_over", self, "_on_game_over")
+    Events.connect("start", self, "_on_start")
 
 
 func shoot():
-    stream = gun_shot
-    play()
+    $Effects.stream = gun_shot
+    $Effects.play()
     
 
 func shoot_and_reload():
@@ -26,8 +31,26 @@ func shoot_and_reload():
     shoot()
 
 
-func _on_finished():
+func _on_effects_finished():
     if reloading:
-        stream = reload
+        $Effects.stream = reload
         reloading = false
-        play()
+        $Effects.play()
+
+
+func _on_music_finished():
+    $Music.play()
+
+
+func _on_happy_pig_slain():
+    $Music.pitch_scale = clamp($Music.pitch_scale + MUSIC_PITCH_DELTA, 1.0, MAX_MUSIC_PITCH)
+
+
+func _on_game_over():
+    $Music.stop()
+
+
+func _on_start():
+    $Music.play(0.0)
+    $Music.pitch_scale = 1.0
+    
