856b82a026b5b1d6d4cdc4599865d60e10067a03
Matt March <matthewmarch29@gmail.com>
2020-06-24 16:31:55 +0100

    Add Godot attribution and license popup

--- UI/CreditsDisplay.gd
+++ UI/CreditsDisplay.gd
@@ -3,7 +3,8 @@ extends Control
 
 func _ready():
     $BackButton.connect("pressed", self, "_on_back_pressed")
-
+    $Godot/GodotLicense.connect("pressed", $LicensePopup, "popup")
+    $LicensePopup/BackButton.connect("pressed", $LicensePopup, "hide")
 
 func _on_back_pressed():
     visible = false
